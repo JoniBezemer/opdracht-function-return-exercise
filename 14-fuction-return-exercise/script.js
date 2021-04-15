9956959e4715faeb3ccd42b0a54fd80bdf06b6c9
JoniBezemer <77683711+JoniBezemer@users.noreply.github.com>
2021-04-15 10:58:12 +0200

    Add files via upload

---
+++ 14-fuction-return-exercise/script.js
@@ -0,0 +1,11 @@
+ function number(a) {
+    const result;
+    if (a > 100){
+        result = 'true';
+    } else {
+        result = 'false';
+    }
+    return result;
+};
+
+console.log(number(101));
